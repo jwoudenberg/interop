bc47382df856a8065b126f21fbc49b4d65eb082a
Jasper Woudenberg <jasper@noredink.com>
2021-03-03 21:01:59 +0100

    Generate ruby method per endpoint

--- src/Interop/Ruby.hs
+++ src/Interop/Ruby.hs
@@ -6,13 +6,17 @@
 module Interop.Ruby (generate) where
 
 import qualified Data.ByteString.Builder as Builder
+import Data.Char as Char
 import Data.Function ((&))
+import qualified Data.HashMap.Strict as HM
 import Data.List (intersperse)
 import Data.Semigroup (stimesMonoid)
 import Data.String (IsString (..))
+import Data.Text (Text)
+import qualified Data.Text as Text
 import Interop
 import qualified System.IO
-import Prelude hiding (return, (>>), (>>=))
+import Prelude hiding (pure, (>>), (>>=))
 
 generate :: FilePath -> Service m -> IO ()
 generate path service =
@@ -23,7 +27,7 @@ generate path service =
         & Builder.hPutBuilder handle
 
 toCode :: Service m -> Ruby
-toCode _ = do
+toCode (Service endpointMap) = do
   "require \"json\""
   "require \"net/http\""
   "require \"uri\""
@@ -39,6 +43,18 @@ toCode _ = do
       "end"
       "@http.use_ssl = @origin.scheme == 'https'"
     "end"
+    foldr (>>) pure (fmap endpoint (HM.keys endpointMap))
+  "end"
+
+endpoint :: Text -> Ruby
+endpoint name = do
+  ""
+  chunks ["def ", toSnakeCase name, "(body:, authorization:)"] $ do
+    "req = Net::HTTP::Post.new(@origin)"
+    "req[\"Content-Type\"] = \"application/json\""
+    "req[\"Authorization\"] = authorization"
+    ""
+    "@http.request(req, body)"
   "end"
 
 -- DSL for generating ruby code from Haskell.
@@ -59,9 +75,35 @@ instance IsString (Ruby -> Ruby) where
         (Ruby (\indent -> block (1 + indent)))
     )
 
+class Chunks a where
+  chunks :: [String] -> a
+
+instance Chunks Ruby where
+  chunks strs = fromString (mconcat strs)
+
+instance Chunks (Ruby -> Ruby) where
+  chunks strs =
+    ( \(Ruby block) -> do
+        chunks strs
+        (Ruby (\indent -> block (1 + indent)))
+    )
+
 Ruby x >> Ruby y = Ruby (x <> y)
 
 pure = ""
 
 render :: Ruby -> Builder.Builder
 render (Ruby f) = f 0
+
+toSnakeCase :: Text -> String
+toSnakeCase text =
+  Text.foldl
+    ( \acc char ->
+        case (acc, Char.isUpper char) of
+          ([], _) -> [Char.toLower char]
+          (_, True) -> Char.toLower char : '_' : acc
+          (_, False) -> char : acc
+    )
+    []
+    text
+    & reverse
