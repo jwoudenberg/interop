5d0552c4b82ec90add5762928763312a07bbaa6b
Jasper Woudenberg <jasper@noredink.com>
2021-04-04 15:10:50 +0200

    Ruby test makes http request

--- test/ruby-tests/test.rb
+++ test/ruby-tests/test.rb
@@ -3,6 +3,18 @@ load 'api.rb'
 
 class TestApi < MiniTest::Unit::TestCase
   def test_api
-    assert_equal "Hi!", "Hi!"
+    api = Api.new("http://localhost:9000")
+    response = api.get_person_by_id(42)
+    expected =
+      Person::Person.new(
+        first_name: "Jasper",
+        las_name: "Woudenberg",
+        hobbies: [
+          Hobby::Hoby.new(
+            description: "boardgames",
+          ),
+        ],
+      )
+    assert_equal response, expected
   end
 end
